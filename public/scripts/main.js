9359781926046eb1873966e578d277ac3a9bcdff
Irwin Tsay <irwintsay@gmail.com>
2017-03-03 11:35:42 -0500

    Update client-side javascript to handle saving books

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -69,9 +69,35 @@ document.addEventListener('DOMContentLoaded', () => {
       method: 'GET',
       url: `/api/googlebooks/?q=${$('input[name=search]').val()}`
     })
-    .then(r => console.log(r));
+    .then(handleSearchResponse);
   };
 
+  const handleSearchResponse = (books) => {
+    $main.empty();
+    $main.show();
+    books.items.forEach(book => {
+      let $bookContainer = $('<div class="book-container">');
+      $bookContainer.append($('<img>').attr('src', book.volumeInfo.imageLinks.thumbnail));
+      $bookContainer.append($('<h2>').text(book.volumeInfo.title));
+      $bookContainer.append($('<h4>').text(book.volumeInfo.subtitle));
+      $bookContainer.append($('<p>').text(`Author: ${book.volumeInfo.authors[0]}`));
+      $bookContainer.append($('<p>').text(`Description: ${book.volumeInfo.description}`));
+      $bookContainer.append(createBookSave(book.volumeInfo));
+      console.log(createBookSave(book.volumeInfo));
+      $main.append($bookContainer);
+    });
+  };
+
+  const createBookSave = (book) => {
+    let $form = $('<form action="/books" method="POST">');
+    $form.append($('<input type="hidden" name="book[thumbnail]">').val(book.imageLinks.thumbnail));
+    $form.append($('<input type="hidden" name="book[title]">').val(book.title));
+    $form.append($('<input type="hidden" name="book[subtitle]">').val(book.subtitle));
+    $form.append($('<input type="hidden" name="book[author]">').val(book.authors[0]));
+    $form.append($('<input type="hidden" name="book[description]">').val(book.description));
+    $form.append($('<input type="submit" value="Save">'));
+    return $form
+  };
 
 
   $loginForm.on('submit', processLogin);
