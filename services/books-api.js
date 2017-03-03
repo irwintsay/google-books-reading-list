c76f909cec2588641416ef5ab7034edc9ed879c8
Irwin Tsay <irwintsay@gmail.com>
2017-03-03 10:43:25 -0500

    Add Google Books server-side fetch

--- services/books-api.js
+++ services/books-api.js
@@ -1,3 +1,10 @@
+const fetch           = require('node-fetch');
+const key             = process.env.GOOGLE_BOOKS_API_KEY;
 let GoogleBooks = {};
+
+GoogleBooks.search = (query) => {
+  let url = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${key}`;
+  return fetch(url);
+};
 
 module.exports = GoogleBooks;
