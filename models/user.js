069b2c370200a63631fd4dd2468d3909fc1fe0ac
Irwin Tsay <irwintsay@gmail.com>
2017-03-03 01:20:11 -0500

    Add User model, update users controller

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const bcrypt      = require('bcrypt');
+const db          = require('../config/database');
+let User          = {};
+
+User.create = (user) => {
+  user.password = bcrypt.hashSync(user.password, 10);
+  return db.one(
+    `INSERT INTO users
+    (first_name, last_name, email, password_digest)
+    VALUES ($1, $2, $3, $4) RETURNING *`,
+    [user.first_name, user.last_name, user.email, user.password]
+  );
+};
+
+User.findByEmail = (email) => {
+  return db.oneOrNone(
+    `SELECT * FROM users
+    WHERE email = $1`,
+    [email]
+  );
+};
+
+module.exports    = User;
